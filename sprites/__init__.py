ca555679e4148f79fa22cb753fb5e1f197aee102
William Goosen <william.goosen@gmail.com>
2025-09-19 12:45:14 +0200

    refactor: extract Powerup class to its own module
    - Moved Powerup class from sprites/sprites.py to sprites/powerup.py
    - Added import for Powerup in sprites/__init__.py
    - Verified powerup spawning and movement

--- sprites/__init__.py
+++ sprites/__init__.py
@@ -2,3 +2,4 @@ from .sprites import *
 from .starfield import Starfield
 from .meteoroid import Meteoroid
 from .bullet import Bullet
+from .powerup import Powerup
--- sprites/sprites.py
+++ sprites/sprites.py
@@ -1,24 +1,5 @@
 import pygame as pg
-from random import choice
 from settings import *
-
-
-class Powerup(pg.sprite.Sprite):
-    def __init__(self, powerup_images, center, screen_width, screen_height):
-        pg.sprite.Sprite.__init__(self)
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.type = choice(["shield_gold", "bolt_gold"])
-        self.image = powerup_images[self.type]
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-        self.speedy = POWERUP_SPEED
-
-    def update(self, dt):
-        self.rect.y += self.speedy * dt
-        if self.rect.top > self.screen_height:
-            self.kill()
 
 
 class Explosion(pg.sprite.Sprite):
