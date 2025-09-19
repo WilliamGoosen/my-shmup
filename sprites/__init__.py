6617cce3ccfbbfbe4ec4354ee93ffbef37778e7b
William Goosen <william.goosen@gmail.com>
2025-09-19 12:41:13 +0200

    refactor: extract Bullet class to its own module
    - Moved Bullet class from sprites/sprites.py to sprites/bullet.py
    - Added import for Bullet in sprites/__init__.py
    - Verified bullet firing, movement, and collision functionality

--- sprites/__init__.py
+++ sprites/__init__.py
@@ -1,3 +1,4 @@
 from .sprites import *
 from .starfield import Starfield
 from .meteoroid import Meteoroid
+from .bullet import Bullet
--- sprites/sprites.py
+++ sprites/sprites.py
@@ -3,21 +3,6 @@ from random import choice
 from settings import *
 
 
-class Bullet(pg.sprite.Sprite):
-    def __init__(self, x, y, bullet_image):
-        pg.sprite.Sprite.__init__(self)
-        self.image = bullet_image
-        self.rect = self.image.get_rect()
-        self.rect.bottom = y
-        self.rect.centerx = x
-        self.speedy = BULLET_SPEED
-
-    def update(self, dt):
-        self.rect.y += self.speedy * dt
-        if self.rect.bottom < 0:
-            self.kill()
-
-
 class Powerup(pg.sprite.Sprite):
     def __init__(self, powerup_images, center, screen_width, screen_height):
         pg.sprite.Sprite.__init__(self)
